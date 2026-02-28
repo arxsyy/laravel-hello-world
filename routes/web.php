617ac6429e54640bf5434514ff87e80325247bc7
arxsyy <marsyfernanda@gmail.com>
2026-02-28 10:45:49 +0700

    JS2_P3_No.6 web

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,10 @@ Route::resource('photos', PhotoController::class);
 Route::resource('photos', PhotoController::class)->only(['index', 'show']);
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
 
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Marsya']);
+// });
+
 Route::get('/greeting', function () {
-    return view('hello', ['name' => 'Marsya']);
+    return view('blog.hello', ['name' => 'Marsyalia Fernanda']);
 });
