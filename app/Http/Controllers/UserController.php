7300c0145c25b44d710768b41ea9cb118d1d69f4
arxsyy <marsyfernanda@gmail.com>
2026-03-28 18:58:41 +0700

    JS3_P6_No.10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,9 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 3
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::insert($data); // tambahkan data ke tabel m_user
+        UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari table m_user
