883a6f79666788c3f2350f531f2a1c3f2eca17a7
arxsyy <marsyfernanda@gmail.com>
2026-04-22 20:20:38 +0700

    JS4_P2.5_No.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,30 @@ class UserController extends Controller
 {
     public function index()
     {
-    $user = UserModel::firstOrNew(
-        [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
             'password' => Hash::make('12345'),
-            'level_id' => 2
-        ],
-    );
-    $user->save();
+            'level_id' => 2,
+        ]);
 
-    return view('user', ['data' => $user]);
+        $user->username = 'manager56';
+
+        $user->isDirty(); 
+        $user->isDirty('username'); 
+        $user->isDirty('nama'); 
+        $user->isDirty(['nama', 'username']); 
+
+        $user->isClean(); 
+        $user->isClean('username'); 
+        $user->isClean('nama'); 
+        $user->isClean(['nama', 'username']); 
+
+        $user->save();
+
+        $user->isDirty(); 
+        $user->isClean(); 
+
+        dd($user->isDirty()); 
     }
 }
