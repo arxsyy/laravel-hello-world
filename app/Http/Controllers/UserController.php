faa35080814f20252861ae838e27ea50f457ba7b
arxsyy <marsyfernanda@gmail.com>
2026-04-22 19:04:34 +0700

    J4_P2.4_No.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate([
-            'username' => 'manager',
-            'nama' => 'Manager',
-        ]);
+        $user = UserModel::firstOrCreate(
+            [
+                'username' => 'manager22',
+                'nama' => 'Manager Dua Dua',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
         return view('user', ['data' => $user]);
     }
 }
