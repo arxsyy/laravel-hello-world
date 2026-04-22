ddb96a99ead1adb2a1f4a471a4289ebbdde7b734
arxsyy <marsyfernanda@gmail.com>
2026-04-22 20:41:50 +0700

    JS4_P2.6_No.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,29 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-
-        $user->username = 'manager12';
-        $user->save();
-
-        $user->wasChanged(); 
-        $user->wasChanged('username'); 
-        $user->wasChanged(['username', 'level_id']); 
-        $user->wasChanged('nama'); 
-        
-        ($user->wasChanged(['nama', 'username'])); 
+        $user = UserModel::all(); 
+        return view('user', ['data' => $user]);
     }
 }
